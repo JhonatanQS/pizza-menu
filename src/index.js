3c097f7d973af0e262a4d62aae20e925c8f31b67
Jhonatan Queiroz <jhonatan.de.queiroz.silva@ibm.com>
2025-08-15 13:22:38 -0400

    Final commit - project completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {pizzaData} from "./data";
 
 
 function App() {
@@ -22,16 +23,53 @@ function Header() {
 function Menu() {
     return <main className="menu">
         <h2>Our menu</h2>
-        <Pizza/>
+
+
+        {pizzaData.length > 0 ? (
+            <>
+                <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic,
+                    all delicious.</p>
+                <ul className="pizzas">
+                    {pizzaData.map((pizza) =>
+                        (<Pizza pizzaObj={pizza} key={pizza.name}/>))}
+                </ul>
+            </>
+
+        ) : (<p>W're still working on our menu. Please come back later :) </p>)}
     </main>
 }
 
 function Footer() {
-    return <footer className="footer">We're currently open!</footer>
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
+    return <footer className="footer">
+        {isOpen ? (
+            <Order closeHour={closeHour} openHour={openHour}/>
+        ) : (<p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>)}
+    </footer>
 }
 
-function Pizza() {
-    return <h3>Pizza</h3>;
+function Order({closeHour, openHour}) {
+    return <div className="order">
+        <p>
+            We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.
+        </p>
+        <button className="btn">Order</button>
+    </div>
+}
+
+function Pizza({pizzaObj}) {
+    return <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+        <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
+        <div>
+            <h3>{pizzaObj.name}</h3>
+            <p>{pizzaObj.ingredients}</p>
+            {pizzaObj.soldOut ? 'SOLD OUT': (<span>{pizzaObj.price}</span>)}
+        </div>
+    </li>;
 }
 
 
